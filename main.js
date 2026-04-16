f185cbe94b02e156e69c58b9d4ae579bcc90bbba
LarissaMonOli <monteiro.oliveira.larissa@escola.pr.gov.br>
2026-04-16 08:40:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+    </section>
+        <script src="main.js"></script>
+</body>
+</html>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
